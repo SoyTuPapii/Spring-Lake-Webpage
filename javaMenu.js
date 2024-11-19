ba6a4c617b2c88e1bd76c14684df2d8de6b7a3e9
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-11-19 11:25:56 -0500

    Added js menu and styles for mobile

---
+++ javaMenu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menuToggle");
+    const dropdownMenu = document.getElementById("dropdownMenu");
+
+    menuToggle.addEventListener("click", function() {
+        dropdownMenu.classList.toggle("show");
+    });
+});
